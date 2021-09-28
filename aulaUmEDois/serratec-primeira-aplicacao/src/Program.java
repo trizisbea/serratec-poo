223fbdfd41108692a9f6238ebe71cd94dbd1b175
Beatriz Nolasco <89035781+trizisbea@users.noreply.github.com>
2021-09-28 15:14:57 -0300

    Add files via upload

---
+++ aulaUmEDois/serratec-primeira-aplicacao/src/Program.java
@@ -0,0 +1,34 @@
+import sala.Cadeira;
+
+/* public class Program {
+
+	public static void main(String[] args) {
+System.out.println("Hello, world! Agora no Eclipse!");
+	}
+
+} */ 
+
+/* public class Program {
+	public static void main (String[] args) {
+		Cadeira cad = new Cadeira(); 
+		cad.Sentar(); 
+	}
+}
+*/ 
+
+/* public class Program {
+
+public static void main(String[] args) {
+	args[0] = "Beatriz"; 
+	
+	System.out.println(args[0]); 
+}
+} */ 
+
+public class Program {
+
+	public static void main(String[] args) {
+System.out.println("Beatriz");
+	}
+}
+	
