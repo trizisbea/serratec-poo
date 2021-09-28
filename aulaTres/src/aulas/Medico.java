ab04144f3d2835aad4ff65337aed16a6f1f942bc
Beatriz Nolasco <89035781+trizisbea@users.noreply.github.com>
2021-09-28 16:18:43 -0300

    Update Medico.java

--- aulaTres/src/aulas/Medico.java
+++ aulaTres/src/aulas/Medico.java
@@ -1,10 +1,10 @@
 package aulas;
 
 	public class Medico {
-	int crm; 
-	String nome; 
-	double salario; 
-	int valorConsulta;
+	private int crm; 
+	private String nome; 
+	private double salario; 
+	private int valorConsulta;
 	private static int total;
 	
 	public Medico(int i, String string, int j, int k) {
@@ -22,20 +22,20 @@ package aulas;
 		
 		double valorDinheiro = 0; 
 		valorDinheiro = (valorConsulta / 1); 
-		System.out.println("Pagamento do mÈdico em consultas em dinheiro: " + valorDinheiro);
+		System.out.println("Pagamento do m√©dico em consultas em dinheiro: " + valorDinheiro);
 	}
 	
 	public void pagamentoPlano() {
 		double valorPlano = 0;
 		valorPlano = (valorConsulta * 0.70);
-		System.out.println("Pagamento do mÈdico em consultas pelo plano: " + valorPlano);
+		System.out.println("Pagamento do m√©dico em consultas pelo plano: " + valorPlano);
 	}
 	
 	public void status() {
-		System.out.println("Dados do mÈdico: ");
+		System.out.println("Dados do m√©dico: ");
 		System.out.println("CRM " + this.crm);
-		System.out.println("Nome do mÈdico: " + this.nome);
-		System.out.println("Sal·rio total: " + this.salario);
+		System.out.println("Nome do m√©dico: " + this.nome);
+		System.out.println("Sal√°rio total: " + this.salario);
 		System.out.println("Valor da Consulta: " + this.valorConsulta);
 	}
 	
